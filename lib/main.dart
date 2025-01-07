51b25658177eb97c864b91c560b8ce626aa03c61
Amir Hossein Mohammadi <amir00462@gmail.com>
2025-01-07 11:31:24 +0330

    full project

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,13 @@ import 'package:flutter_state_managment/features/product/screens/product_list_sc
 import 'package:flutter_state_managment/providers/product_provider.dart';
 import 'package:provider/provider.dart';
 
-// add provider :
-// 1. use MultiProvider as root
-// 2. create Provider for each state
-// 3. add providers created to multiprovider
-// 4. use (  )
+// Use Provider StateManagment :
+// 1. add Provider package to project
+// 2. use MultiProvider as root
+// 3. create Provider for each state
+// 4. add providers created to multiprovider
+// 5. use Provider.of -> when you do not need a rebuild in current ui
+// 6. use Consumer -> when you need a rebuild in current ui
 
 void main() {
   runApp(MyApp());
